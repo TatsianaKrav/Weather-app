23cf8437ff99b2c49fd1668b14acc7eeee88c5ca
Tatiana <tats.krauchanka@gmail.com>
2025-04-30 18:24:49 +0300

    feat: add weather request

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,9 @@
-import { AfterContentChecked, AfterViewChecked, AfterViewInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { debounceTime } from 'rxjs';
 import { CitySearchService } from '../../services/city-search.service';
-import { CityInfoResponse } from '../../models/city-info-response';
+import { WeatherResponse } from '../../models/weather-response';
+
 
 @Component({
   selector: 'app-search',
@@ -14,6 +15,7 @@ import { CityInfoResponse } from '../../models/city-info-response';
 export class SearchComponent {
   protected readonly searchControl = new FormControl('');
   dropdownOptions: string[] = [];
+  weatherInfo: WeatherResponse | null = null;
 
   constructor(private citySearchService: CitySearchService) {
     this.searchControl.valueChanges
@@ -22,13 +24,26 @@ export class SearchComponent {
       )
       .subscribe(value => {
         if (value) {
-          this.citySearchService.getWeatherByCity(value).subscribe(data => {
+          this.citySearchService.getCityInfo(value).subscribe(data => {
             this.dropdownOptions = data.map(cityObj => cityObj.name);
+
+            const cityToFind = data.find(city => city.name === value);
+
+            if (cityToFind) {
+
+              const lat = cityToFind.lat;
+              const lon = Number(cityToFind.lon);
+
+              this.citySearchService.getWeatherByCity(lat, lon).subscribe(data => {
+                if (data) {
+                  this.weatherInfo = data;
+                }
+              })
+            }
           })
         }
       })
   }
-
 
 }
 
--- src/app/services/city-search.service.ts
+++ src/app/services/city-search.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Observable } from 'rxjs';
 import { CityInfoResponse } from '../models/city-info-response';
+import { WeatherResponse } from '../models/weather-response';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,12 @@ export class CitySearchService {
   constructor(private http: HttpClient) {
   }
 
-  getWeatherByCity(cityName: string): Observable<CityInfoResponse[]> {
+  getCityInfo(cityName: string): Observable<CityInfoResponse[]> {
     return this.http.get<CityInfoResponse[]>(`http://api.openweathermap.org/geo/1.0/direct?q={${cityName}}&limit=10&appid=${environment.API_KEY}`)
+  }
+
+
+  getWeatherByCity(latitude: number, longitud: number): Observable<WeatherResponse> {
+    return this.http.get<WeatherResponse>(`https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitud}&appid=${environment.API_KEY}&cnt=1`)
   }
 }
